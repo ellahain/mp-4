fe0a25da7c03d0242a509908f117501d951251d1
ella hain <ehain@bu.edu>
2025-10-30 20:17:44 -0400

    updated error handling fix error

--- lib/getData.ts
+++ lib/getData.ts
@@ -5,8 +5,9 @@ export default async function getData() {
     /*Learned how to do _exists_ from https://www.elastic.co/guide/en/elasticsearch/reference/7.17/query-dsl-query-string-query.html*/
 
     /*try/catch for error handling*/
+    const res;
     try {
-        const res = await fetch(`https://api.harvardartmuseums.org/object?q=imagepermissionlevel:0+_exists_:primaryimageurl&apikey=${API_KEY}`)
+        res = await fetch(`https://api.harvardartmuseums.org/object?q=imagepermissionlevel:0+_exists_:primaryimageurl&apikey=${API_KEY}`)
     } catch {
         throw new Error(res.statusText);
     }
